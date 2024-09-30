5b50414c4a9cb5f4f3a0058cfa5f5802bcd6e99f
Valentin SEURIN <valentin.seurin@beecoming.fr>
2024-09-30 17:16:11 +0200

    D1

---
+++ src/app/left-container/left-container.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  imports: [FontAwesomeModule],
+  selector: 'app-left-container',
+  templateUrl: './left-container.component.html',
+  styleUrl: './left-container.component.css',
+  standalone: true,
+})
+export class LeftContainerComponent {
+  faMagnifyingGlass: any = faMagnifyingGlass;
+  faLocation: any = faLocation;
+}
---
+++ src/app/right-container/right-container.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-container.component.html',
+  styleUrl: './right-container.component.css',
+})
+export class RightContainerComponent {
+  today: boolean = false;
+  week: boolean = true;
+  celsius: boolean = true;
+  fahrenheit: boolean = false;
+
+  OnTodayClick() {
+    this.today = true;
+    this.week = false;
+  }
+  OnWeekClick() {
+    this.today = false;
+    this.week = true;
+  }
+  onCelsiusClick() {
+    this.celsius = true;
+    this.fahrenheit = false;
+  }
+
+  onFahrenheitClick() {
+    this.celsius = false;
+    this.fahrenheit = true;
+  }
+}
